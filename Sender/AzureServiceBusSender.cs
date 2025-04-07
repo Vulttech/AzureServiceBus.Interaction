c4105cd12f89b82c8b412a59345a3d5ff942c9f5
Federico Celico <federico.celico87@gmail.com>
2025-04-07 17:10:28 -0300

    Initial Commit

---
+++ Sender/AzureServiceBusSender.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AzureServiceBus.Interaction.Sender
+{
+    public class AzureServiceBusSender
+    {
+        private readonly ServiceBusClient _client;
+
+        public AzureServiceBusSender(ServiceBusClient client)
+        {
+            _client = client;
+        }
+
+        public async Task SendMessageAsync<T>(string QueueName, T message)
+        {
+            var sender = _client.CreateSender(QueueName);
+            var json = JsonSerializer.Serialize(message);
+            var sbMessage = new ServiceBusMessage(json);
+            await sender.SendMessageAsync(sbMessage);
+        }
+    }
+}
---
+++ Sender/IMessageSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBus.Interaction.Sender
+{
+    public interface IMessageSender
+    {
+        Task SendMessageAsync<T>(string QueueName, T message);
+    }
+}
