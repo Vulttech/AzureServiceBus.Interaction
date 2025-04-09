1a305834e468e6bb0ffa3595297aa1ef2664e8a5
Federico Celico <federico.celico87@gmail.com>
2025-04-09 12:08:07 -0300

    Fix Interface

---
+++ Handlers/IAzureMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBus.Interaction.Handlers
+{
+    public interface IAzureMessageHandler<T>
+    {
+        Task HandleAsync(T message, CancellationToken cancellationToken);
+    }
+}
